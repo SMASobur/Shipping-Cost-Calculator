eb5a0e3dbd1e55213d0c534b2bfe38ca3a700afb
SMASobur <sikdar.se@gmail.com>
2026-05-07 15:49:52 +0200

    Add PostConstruct and Component annotation

--- src/main/java/se/lexicon/calculator/ExpressInternationalShipping.java
+++ src/main/java/se/lexicon/calculator/ExpressInternationalShipping.java
@@ -1,18 +1,28 @@
 package se.lexicon.calculator;
 
-import org.springframework.stereotype.Component;
 import se.lexicon.model.Destination;
 import se.lexicon.model.ShippingRequest;
 import se.lexicon.model.Speed;
 import se.lexicon.service.ShippingCostCalculator;
+
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
+
 @Component
 public class ExpressInternationalShipping implements ShippingCostCalculator {
 
+    @Override
     public boolean supports(ShippingRequest r) {
         return r.destination() == Destination.INTERNATIONAL && r.speed() == Speed.EXPRESS;
     }
 
+    @Override
     public double calculate(ShippingRequest r) {
         return 25 + 4.5 * r.weightKg();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean " + this.getClass().getSimpleName() + " has been created");
     }
 }
--- src/main/java/se/lexicon/calculator/StandardDomesticShipping.java
+++ src/main/java/se/lexicon/calculator/StandardDomesticShipping.java
@@ -1,17 +1,28 @@
 package se.lexicon.calculator;
 
-import org.springframework.stereotype.Component;
 import se.lexicon.model.Destination;
 import se.lexicon.model.ShippingRequest;
 import se.lexicon.model.Speed;
 import se.lexicon.service.ShippingCostCalculator;
+
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
+
 @Component
 public class StandardDomesticShipping implements ShippingCostCalculator {
+
+    @Override
     public boolean supports(ShippingRequest r) {
         return r.destination() == Destination.DOMESTIC && r.speed() == Speed.STANDARD;
     }
 
+    @Override
     public double calculate(ShippingRequest r) {
         return 5 + 1.2 * r.weightKg();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean " + this.getClass().getSimpleName() + " has been created");
     }
 }
